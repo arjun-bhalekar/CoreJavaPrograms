024d8fb8f0262e81b64e0a910212ffd2f3c7d89a
Arjun Bhalekar <arjunbhalekar95@gmail.com>
2024-02-10 13:06:48 +0530

    initial project code

---
+++ src/com/core/MainApplication.java
@@ -0,0 +1,10 @@
+package com.core;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("====== Welcome to Core Java Programs Application =====");
+
+    }
+}
